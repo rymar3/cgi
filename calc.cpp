d71ae991b54d22f9a7e787819f0c969027c0a506
rymar4 <rymar24@ukr.net>
2022-05-11 19:53:16 +0300

    Start calc 2

--- calc.cpp
+++ calc.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(void) {
 printf("Content-Type: text/html\n\n");
-printf("<h1 align=center>Hello world FROM CPP </H1>");
-printf("<h1 align=center>PROGRAM CPP </H1>");
+printf("<h1 align=center>Hello world FROM RYMAR </H1>");
+printf("<h3 align=center>PROGRAM CPP </H3>");
 return 0;
 }
