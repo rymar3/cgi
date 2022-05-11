fc6f51a0eb9a23da60f36639e6b35114d2887156
rymar4 <rymar24@ukr.net>
2022-05-11 20:08:12 +0300

    Добавив форму calc 4

--- calc.cpp
+++ calc.cpp
@@ -7,6 +7,26 @@ cout << "Content-Type: text/html\n\n";
 
 cout << "<html><head><meta charset=utf8><title>Калькулятор</title></head><body>\n";
 cout << "<h1 align=center><font size=10 color=green>Калькулятор</font></h1>\n";
-
+cout << "<form action='form.cgi' method='post'>\n";
+  cout << "<TABLE align='center' bgcolor='green'>\n";
+  cout << "<TR>\n";
+  cout << "<TD><font color='white'>Перше число :</font>: </TD>\n";
+  cout << "<TD><input type='text' size='10' maxlength='20' name='num1'> </TD>\n";
+  cout << "</TR>\n";
+  cout << "<TR>\n";
+  cout << "<TD><font color='white'>Друге число :</font> :  </TD>\n";
+  cout << "<TD><input type='text' size='10' maxlength='20' name='num2'> </TD>\n";
+  cout << "</TR>\n";
+  cout << "<TR>\n";
+  cout << "<TD><font color='white'>Операція :</font> : </TD>\n";
+  cout << "<TD><input type='text' size='10' maxlength='20' name='op1'></TD>\n";
+  cout << "</TR>\n";
+  cout << "</TABLE>\n";
+  cout << "<p align='center'>\n";
+  cout << "<input type='submit' value='Вирахувати' >\n";
+  cout << "<input type='reset' value='Очистити'>\n";
+  cout << "</p>\n";
+   cout << "</form>\n";
+  cout << "<hr>\n";
 return 0;
 }
