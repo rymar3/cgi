a3033041123d758629e5300843bcb6078131e5a9
rymar4 <rymar24@ukr.net>
2022-05-11 19:12:53 +0300

    Start calc

---
+++ calc.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(void) {
+printf("Content-Type: text/html\n\n");
+printf("<h1 align=center>Hello world FROM CPP </H1>");
+printf("<h1 align=center>PROGRAM CPP </H1>");
+return 0;
+}
