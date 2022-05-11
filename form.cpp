932476f644eadb164350d187e3f0cd1c2b0e93fd
rymar4 <rymar24@ukr.net>
2022-05-11 20:18:17 +0300

    Добавив form calc 5

---
+++ form.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cgicc/CgiDefs.h>
+#include <cgicc/Cgicc.h>
+#include <cgicc/HTTPHTMLHeader.h>
+#include <cgicc/HTMLClasses.h>
+using namespace std;
+using namespace cgicc;
+int main() {
+  Cgicc cgi;
+ string op1;
+ string nm1;
+ string nm2;
+  nm1 = cgi("num1");
+  nm2 = cgi("num2");
+  op1 = cgi("op1");
+cout << "<H1 align='center'> <font color='green'>" << num1 << op1 << num2 << "= 12" << "</font></H1>\n";
+return 0
+}
