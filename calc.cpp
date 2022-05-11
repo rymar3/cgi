b53134b87a9f3c7e104a24ec2bf23a1d99d9f916
rymar4 <rymar24@ukr.net>
2022-05-11 20:01:16 +0300

    Добавив заголовок calc 3

--- calc.cpp
+++ calc.cpp
@@ -1,10 +1,11 @@
-#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
 int main(void) {
 cout << "Content-Type: text/html\n\n";
 
 cout << "<h1 align=center><font size=5 color=green>Калькулятор</font></h1>\n";
-
-
 
 return 0;
 }
