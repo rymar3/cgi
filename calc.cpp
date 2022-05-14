56038590e7eb39d79063582a90d32f5273758d4a
rymar4 <rymar4@ukr.net>
2022-05-14 16:13:00 +0300

    Calc 1

---
+++ calc.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+using namespace std;
+int main() {
+  cout << "Content-type: text/html\r\n\r\n";
+  cout << "<!doctype html>\n";
+  cout << "<head>\n";
+  cout << "<title>Калькулятор</title>\n";
+  cout << "<meta charset=UTF-8>\n";
+
+  cout << "</head>\n";
+  cout << "<body>\n";
+
+  cout << "<HR>\n";
+  cout << "<H1 align='center'><font color='green'>Калькулятор :</font></H1>\n";
+  cout << "<form action='form.cgi' method='post'>\n";
+  cout << "<TABLE align='center' bgcolor='green'>\n";
+  cout << "<TR>\n";
+  cout << "<TD><font color='white'>Перше число :</font>: </TD>\n";
+  cout << "<TD><input type='text' size='10' maxlength='20' name='num1'> </TD>\n";
+  cout << "</TR>\n";
+  cout << "<TR>\n";
+  cout << "<TD><font color='white'>Друге число :</font> :  </TD>\n";
+  cout << "<TD><input type='text' size='10' maxlength='20' name='num2'> </TD>\n";
+  cout << "</TR>\n";
+  cout << "<TR>\n";
+  cout << "<TD><font color='white'>Операція :</font> : </TD>\n";
+  cout << "<TD><input type='text' size='10' maxlength='20' name='op1'></TD>\n";
+  cout << "</TR>\n";
+  cout << "</TABLE>\n";
+  cout << "<p align='center'>\n";
+  cout << "<input type='submit' value='Вирахувати' >\n";
+  cout << "<input type='reset' value='Очистити'>\n";
+  cout << "</p>\n";
+   cout << "</form>\n";
---
+++ form.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <stdio.h>
+#include <stdlib.h>
+#include <cgicc/CgiDefs.h>
+#include <cgicc/Cgicc.h>
+#include <cgicc/HTTPHTMLHeader.h>
+#include <cgicc/HTMLClasses.h>
+using namespace std;
+using namespace cgicc;
+int main() {
+  Cgicc cgi;
+  string op1;
+ string nm1;
+ string nm2;
+  cout << "Content-type: text/html\r\n\r\n";
+  cout << "<!doctype html>\n";
+  cout << "<html lang=\"en\">\n";
+  cout << "<head>\n";
+  cout << "<title>Hello World</title>\n";
+  cout << "<meta charset=UTF-8>\n";
+  cout << "</head>\n";
+  cout << "<body>\n";
+  cout << "<HR>\n";
+  cout << "<H1 align='center'><font color='green'>Результат :</font></H1>\n";
+  cout << "<p align=center>";
+  nm1 = cgi("num1");
+  nm2 = cgi("num2");
+  op1 = cgi("op1");
+
+  if (!nm1.empty() && !nm2.empty() && !op1.empty()) {
+std::string si = nm1;
+std::string sf = nm2;
+int num1 = atoi(si.c_str());
+int num2 = atoi(sf.c_str());
+
+if(op1=="*") cout << "<H1 align='center'> <font color='green'>" << num1 << op1 << num2 << "= " << (n
+if(op1=="/") cout << "<H1 align='center'> <font color='green'>" << num1 << op1 << num2 << "= " << (n
+if(op1=="+") cout << "<H1 align='center'> <font color='green'>" << num1 << op1 << num2 << "= " << (n
+if(op1=="-") cout << "<H1 align='center'> <font color='green'>" << num1 << op1 << num2 << "= " << (n
+
+  } else {.
+    cout << "Не всі числа введені !.";
+  }
+  cout << "</p>\n";
+  cout << "<hr>\n";
+  cout << "</body>\n";
+  cout << "</html>";
+  return 0;
+}
