3c9cfc093528761b34ad8e6e4018fd949c27e39f
rymar4 <rymar4@ukr.net>
2022-05-14 16:07:46 +0300

    Start 2

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
 using namespace std;int main()
 {
     cout <<"Content-Type: text/html\n\n"<< endl;
-cout <<"\n<H1 align=center>Hello World</H1>\n\n"<< endl;
+cout <<"\n<H1 align=center>Hello World - Rymar</H1>\n\n"<< endl;
 return 0;
 }
 
